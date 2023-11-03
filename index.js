154d0438c2a42c6c5bd4c4f38861cc2b9fc907e5
Tanjil Ahamed <tanjil113355@gmail.com>
2023-11-03 15:40:03 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken')
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
+
+
+
+const verifyToken = async (req, res, next) => {
+
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized' })
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'forbidden' })
+        }
+        req.user = decoded
+        next()
+    })
+
+}
+
+
+
 
 app.get('/', (req, res) => {
     res.send('this is home page')
 })
-
-// digitalShop
-// 9lj0VvwweYHh418T
-
-
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const uri = "mongodb+srv://<username>:<password>@cluster0.8mn4lkn.mongodb.net/?retryWrites=true&w=majority";
@@ -37,6 +61,28 @@ async function run() {
         const database = client.db("digitalDB");
         const shirtCollection = database.collection("shirt");
         const customerProductsCollection = database.collection("customerProducts");
+
+
+        // token related
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false
+                })
+                .send({ success: true })
+        })
+
+
+        app.post('/logout', async (req, res) => {
+
+            res.clearCookie('token', { maxAge: 0 }).send({ logout: true })
+        })
+
+
+
 
         app.post('/shirt', async (req, res) => {
             const newProduct = req.body
@@ -89,8 +135,13 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/myproducts', async (req, res) => {
+        app.get('/myproducts', verifyToken, async (req, res) => {
+
+            const user = req.user
             const userEmail = req.query.email
+            if (user.email !== userEmail) {
+                return res.send({ message: 'unauthorized'})
+            }
             const query = { email: userEmail }
             const result = await customerProductsCollection.find(query).toArray()
             res.send(result)
