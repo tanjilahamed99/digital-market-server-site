58ef193e46c3982cdee6cd4d5bf196a5b29166a4
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-19 18:26:26 +0600

    simple commit

--- src/api/NewsLatter/newsLatter.js
+++ src/api/NewsLatter/newsLatter.js
@@ -2,8 +2,13 @@ const NewsLatter = require("../../modals/newsLatter/newsLatter")
 
 const postNewsLatterCon = async (req, res) => {
     const email = req.body.email
-    const result = await NewsLatter.create({ email })
-    res.send(result)
+    const query = { email: email }
+    const isExist = await NewsLatter.findOne(query)
+    if (!isExist) {
+        const result = await NewsLatter.create({ email })
+        return res.send(result)
+    }
+    return res.send({ exist: true })
 
 }
 
